05bb9c7e97b76e7c8f0da0d77604ae5cd2a877ff
lykakr <lykeio.akraifnio@gmail.com>
2026-04-17 09:20:10 +0300

    Αρχικά αρχεία για έργο MakeCode

---
+++ main.ts
@@ -0,0 +1,15 @@
+for (let index = 0; index < 3; index++) {
+    basic.showLeds(`
+        # # # # #
+        # . . . #
+        # . . . #
+        # . . . #
+        # # # # #
+        `)
+    basic.pause(1000)
+    basic.clearScreen()
+    basic.pause(1000)
+}
+basic.forever(function () {
+	
+})
